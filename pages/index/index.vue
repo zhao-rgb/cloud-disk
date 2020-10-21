31f2102fc8a9de9b074d5172c122ab4f0efb5d7d
zhao-rgb <1019916061@qq.com>
2020-10-21 09:28:51 +0800

    去掉首页默认导航，引入uni-app的状态栏组件，在页面中添加自定义导航内容

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,23 +1,29 @@
 <template>
-	
+	<view>
+		<uni-status-bar></uni-status-bar>
+		<view style="height: 44px;" class="flex border-bottom align-center">
+			<view class="flex-1 flex"><text class="font-md ml-3">首页</text></view>
+			<view class="flex-1 flex justify-center"></view>
+			<view class="flex-1 flex justify-end">
+				<view style="width: 60rpx; height: 60rpx;" class="flex align-center justify-center bg-light rounded-circle mr-3"><text class="iconfont icon-zengjia"></text></view>
+				<view style="width: 60rpx; height: 60rpx;" class="flex align-center justify-center bg-danger rounded-circle mr-3"><text class="iconfont icon-gengduo"></text></view>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+import uniStatusBar from '@/components/uni-ui/uni-status-bar/uni-status-bar.vue';
+export default {
+	components: {
+		uniStatusBar
+	},
+	data() {
+		return {};
+	},
+	onLoad() {},
+	methods: {}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
