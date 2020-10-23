a728b34f3ed2fd62d8b11e3eed29eb605fa5f82b
zhao-rgb <1019916061@qq.com>
2020-10-23 10:50:50 +0800

    图片预览功能

--- components/common/f-list.vue
+++ components/common/f-list.vue
@@ -1,61 +1,68 @@
 <template>
-	<view class="p-3 flex align-center border-bottom border-light-secondary">
+	<view class="p-3 flex align-center border-bottom border-light-secondary" @click="$emit('click')">
 		<text class="iconfont" :class="iconClass" style="font-size: 60rpx;"></text>
 		<view class="flex flex-column ml-3" style="line-height: 1.2;">
-			<text class="font-md">{{item.name}}</text>
-			<text class="font-sm text-muted">{{item.create_time}}</text>
+			<text class="font-md">{{ item.name }}</text>
+			<text class="font-sm text-muted">{{ item.create_time }}</text>
 		</view>
-		<view class="ml-auto flex align-center justify-center" style="width: 70rpx; height: 70rpx;" @click.stop="select">
-			<text v-if="!item.checked" style="height: 40rpx; width: 40rpx;" class="rounded-circle border border-dark"></text>
+		<view
+			class="ml-auto flex align-center justify-center"
+			style="width: 70rpx; height: 70rpx;"
+			@click.stop="select"
+		>
+			<text
+				v-if="!item.checked"
+				style="height: 40rpx; width: 40rpx;"
+				class="rounded-circle border border-dark"
+			></text>
 			<text v-else class="iconfont icon-xuanze-yixuan text-primary" style="font-size: 40rpx;"></text>
 		</view>
 	</view>
 </template>
 
 <script>
-	const icons = {
-		dir: {
-			icon: 'icon-file-b-2',
-			color: 'text-warning'
-		},
-		image: {
-			icon: 'icon-file-b-6',
-			color: 'text-success'
-		},
-		video: {
-			icon: 'icon-file-b-9',
-			color: 'text-primary'
-		},
-		text: {
-			icon: 'icon-file-s-7',
-			color: 'text-info'
-		},
-		none: {
-			icon: 'icon-file-b-8',
-			color: 'text-muted'
+const icons = {
+	dir: {
+		icon: 'icon-file-b-2',
+		color: 'text-warning'
+	},
+	image: {
+		icon: 'icon-file-b-6',
+		color: 'text-success'
+	},
+	video: {
+		icon: 'icon-file-b-9',
+		color: 'text-primary'
+	},
+	text: {
+		icon: 'icon-file-s-7',
+		color: 'text-info'
+	},
+	none: {
+		icon: 'icon-file-b-8',
+		color: 'text-muted'
+	}
+};
+export default {
+	props: {
+		item: Object,
+		index: [Number, String]
+	},
+	computed: {
+		iconClass() {
+			let item = icons[this.item.type];
+			return `${item.icon} ${item.color}`;
 		}
-	};
-	export default {
-		props: {
-			item: Object,
-			index: [Number, String]
-		},
-		computed: {
-			iconClass() {
-				let item = icons[this.item.type];
-				return `${item.icon} ${item.color}`;
-			}
-		},
-		methods: {
-			select() {
-				this.$emit('select', {
-					index: this.index,
-					value: !this.item.checked
-				});
-			}
+	},
+	methods: {
+		select() {
+			this.$emit('select', {
+				index: this.index,
+				value: !this.item.checked
+			});
 		}
 	}
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -5,11 +5,17 @@
 			<template v-if="checkCount == 0">
 				<text slot="left" class="font-md ml-3">首页</text>
 				<template slot="right">
-					<view style="width: 60rpx; height: 60rpx;" class="flex align-center justify-center bg-icon rounded-circle mr-3"
-					 @tap="openAddDialog">
+					<view
+						style="width: 60rpx; height: 60rpx;"
+						class="flex align-center justify-center bg-icon rounded-circle mr-3"
+						@tap="openAddDialog"
+					>
 						<text class="iconfont icon-zengjia"></text>
 					</view>
-					<view style="width: 60rpx; height: 60rpx;" class="flex align-center justify-center bg-icon rounded-circle mr-3">
+					<view
+						style="width: 60rpx; height: 60rpx;"
+						class="flex align-center justify-center bg-icon rounded-circle mr-3"
+					>
 						<text class="iconfont icon-gengduo"></text>
 					</view>
 				</template>
@@ -17,15 +23,22 @@
 
 			<template v-else>
 				<view slot="left" class="font-md ml-3 text-primary" @click="handleCheckAll(false)">取消</view>
-				<view class="font-md font-weight-bold">已选中{{checkCount}}个</view>
+				<view class="font-md font-weight-bold">已选中{{ checkCount }}个</view>
 				<view slot="right" class="font-md ml-3 text-primary" @click="handleCheckAll(true)">全选</view>
 			</template>
-
 		</nav-bar>
 		<view style="height: 1000px;">
 			<!-- 搜索框，设置圆角 -->
 			<uni-search-bar :radius="100"></uni-search-bar>
-			<f-list v-for="(item, index) in list" :key="index" :item="item" :index="index" @select="select"></f-list>
+			<!-- 列表 -->
+			<f-list
+				v-for="(item, index) in list"
+				:key="index"
+				:item="item"
+				@click="doEvent(item)"
+				:index="index"
+				@select="select"
+			></f-list>
 		</view>
 
 		<!-- 底部操作条 -->
@@ -36,10 +49,16 @@
 			<!-- 操作条容器的样式，高度，颜色，固定在底部，垂直方向拉升效果 -->
 			<view style="115rpx" class="flex align-stretch bg-primary text-white fixed-bottom">
 				<!-- 根据元素个数等分容器，所以要么四个等分，要么两个等分，行高的修改可以让图标和文字之间的距离变得合理，点击还会变色:hover-class -->
-				<view class="flex-1 flex flex-column align-center justify-center" style="line-height: 1.5;" v-for="(item,index) in actions"
-				 :key="index" hover-class="bg-hover-primary" @click="handleBottomEvent(item)">
+				<view
+					class="flex-1 flex flex-column align-center justify-center"
+					style="line-height: 1.5;"
+					v-for="(item, index) in actions"
+					:key="index"
+					hover-class="bg-hover-primary"
+					@click="handleBottomEvent(item)"
+				>
 					<text class="iconfont" :class="item.icon"></text>
-					{{item.name}}
+					{{ item.name }}
 				</view>
 			</view>
 		</view>
@@ -48,21 +67,43 @@
 		<f-dialog ref="dialog">是否删除选中的文件？</f-dialog>
 
 		<!-- 重命名，通过ref定义不同的对话框对象，不同操作弹出的dialog是不同的对象 -->
-		<f-dialog ref="rename"><input type="text" v-model="renameValue" class="flex-1 bg-light rounded px-2" style="height: 95rpx;"
-			 placeholder="重命名" /></f-dialog>
+		<f-dialog ref="rename">
+			<input
+				type="text"
+				v-model="renameValue"
+				class="flex-1 bg-light rounded px-2"
+				style="height: 95rpx;"
+				placeholder="重命名"
+			/>
+		</f-dialog>
 
 		<!-- 新建文件夹，使用自定义弹出层，使用input作为插槽，绑定data中的newdirname变量 -->
-		<f-dialog ref="newdir"><input type="text" v-model="newdirname" class="flex-1 bg-light rounded px-2" style="height: 95rpx;"
-			 placeholder="新建文件夹名称" /></f-dialog>
+		<f-dialog ref="newdir">
+			<input
+				type="text"
+				v-model="newdirname"
+				class="flex-1 bg-light rounded px-2"
+				style="height: 95rpx;"
+				placeholder="新建文件夹名称"
+			/>
+		</f-dialog>
 		<!-- 添加操作条，应当能理解这里ref的作用了，type表示弹出层的位置类型，具体取值都在popup子组件中 -->
 		<uni-popup ref="add" type="bottom">
 			<view class="bg-white flex" style="height: 200rpx;">
 				<!-- 遍历addList数组，纵向flex，为每个操作分配等高的空间，每个操作有图标和名称组成 -->
-				<view class="flex-1 flex align-center justify-center flex-column" hover-class="bg-light" v-for="(item, index) in addList"
-				 :key="index" @tap="handleAddEvent(item)">
+				<view
+					class="flex-1 flex align-center justify-center flex-column"
+					hover-class="bg-light"
+					v-for="(item, index) in addList"
+					:key="index"
+					@tap="handleAddEvent(item)"
+				>
 					<!-- 每个操作的图标，可以传入图标的名称和颜色，很灵活 -->
-					<text style="width: 110rpx;height: 110rpx;" class="rounded-circle bg-light iconfont flex align-center justify-center"
-					 :class="item.icon + ' ' + item.color"></text>
+					<text
+						style="width: 110rpx;height: 110rpx;"
+						class="rounded-circle bg-light iconfont flex align-center justify-center"
+						:class="item.icon + ' ' + item.color"
+					></text>
 					<!-- 每个操作系统 -->
 					<text class="font text-muted">{{ item.name }}</text>
 				</view>
@@ -72,196 +113,223 @@
 </template>
 
 <script>
-	import uniSearchBar from '@/components/uni-ui/uni-search-bar/uni-search-bar.vue'
-	import navBar from '@/components/common/nav-bar.vue'
-	import fList from '@/components/common/f-list.vue'
-	import fDialog from '@/components/common/f-dialog.vue'
-	import uniPopup from '@/components/uni-ui/uni-popup/uni-popup.vue';
-	export default {
-		components: {
-			uniSearchBar,
-			navBar,
-			fList,
-			fDialog,
-			uniPopup
-		},
-		data() {
-			return {
-				renameValue: '',
-				newdirname: '',
-				list: [{
-						type: 'dir',
-						name: '我的笔记',
-						create_time: '2020-10-21 08:00',
-						checked: false
-					},
-					{
-						type: 'image',
-						name: '风景.jpg',
-						create_time: '2020-10-21 08:00',
-						checked: false
-					},
-					{
-						type: 'video',
-						name: 'uniapp实战教程.mp4',
-						create_time: '2020-10-21 08:00',
-						checked: false
-					},
-					{
-						type: 'text',
-						name: '记事本.txt',
-						create_time: '2020-10-21 08:00',
-						checked: false
-					},
-					{
-						type: 'none',
-						name: '压缩包.rar',
-						create_time: '2020-10-21 08:00',
-						checked: false
-					}
-				],
-				addList: [{
-						icon: 'icon-file-b-6',
-						color: 'text-success',
-						name: '上传图片'
-					},
-					{
-						icon: 'icon-file-b-9',
-						color: 'text-primary',
-						name: '上传视频'
-					},
-					{
-						icon: 'icon-file-b-8',
-						color: 'text-muted',
-						name: '上传文件'
-					},
-					{
-						icon: 'icon-file-b-2',
-						color: 'text-warning',
-						name: '新建文件夹'
-					}
-				]
-			};
-		},
-		onLoad() {},
-		methods: {
-			select(e) {
-				//接收到子组件传递过来的索引选中状态，将对应的list中的数据更新
-				this.list[e.index].checked = e.value
-			},
-			//全选/取消全选
-			handleCheckAll(checked) {
-				this.list.forEach(item => {
-					item.checked = checked;
-				});
-			},
-			//处理底部操作条事件，这里仅对"删除"做了处理
-			handleBottomEvent(item) {
-				switch (item.name) {
-					case '删除':
-						this.$refs.dialog.open(close => {
-							//对list进行过滤，留下未被选中的
-							this.list = this.list.filter(item => !item.checked);
-							close();
-							uni.showToast({
-								title: '删除成功',
-								icon: 'none'
-							})
-							// // 在这儿可以写点击删除需要做的回调事件，这里先在控制台模拟，实际需要把checkList移除掉
-							// console.log('删除文件');
-							// console.log(this.checkList);
-						});
-						break;
-					case '重命名':
-						//重命名只能对单个文件进行，所以取this.checkList[0],也就是选中的唯一元素
-						this.renameValue = this.checkList[0].name;
-						this.$refs.rename.open(close => {
-							if (this.renameValue == '') {
-								return uni.showToast({
-									title: '文件名称不能为空',
-									icon: 'none'
-								});
-							}
-							//更新钙元素的name值，实时看到效果
-							this.checkList[0].name = this.renameValue;
-							close();
-						});
-						break;
-					default:
-						break;
+import uniSearchBar from '@/components/uni-ui/uni-search-bar/uni-search-bar.vue';
+import navBar from '@/components/common/nav-bar.vue';
+import fList from '@/components/common/f-list.vue';
+import fDialog from '@/components/common/f-dialog.vue';
+import uniPopup from '@/components/uni-ui/uni-popup/uni-popup.vue';
+export default {
+	components: {
+		uniSearchBar,
+		navBar,
+		fList,
+		fDialog,
+		uniPopup
+	},
+	data() {
+		return {
+			renameValue: '',
+			newdirname: '',
+			list: [
+				{
+					type: 'dir',
+					name: '我的笔记',
+					create_time: '2020-10-21 08:00',
+					checked: false
+				},
+				{
+					type: 'image',
+					name: '风景.jpg',
+					data: 'https://kkkksslls.oss-cn-beijing.aliyuncs.com/campus/E0867EEDD11833E76C12DAD37F603911.jpg',
+					// create_time: '2020-10-21 08:00',
+					checked: false
+				},
+				{
+					type: 'video',
+					name: 'uniapp实战教程.mp4',
+					create_time: '2020-10-21 08:00',
+					checked: false
+				},
+				{
+					type: 'text',
+					name: '记事本.txt',
+					create_time: '2020-10-21 08:00',
+					checked: false
+				},
+				{
+					type: 'none',
+					name: '压缩包.rar',
+					create_time: '2020-10-21 08:00',
+					checked: false
 				}
-			},
-			//打开添加操作条
-			openAddDialog() {
-				this.$refs.add.open();
-			},
-			//处理添加操作条的各种事件
-			handleAddEvent(item) {
-				this.$refs.add.close();
-				switch (item.name) {
-					case '新建文件夹':
-						this.$refs.newdir.open(close => {
-							if (this.newdirname == '') {
-								return uni.showToast({
-									title: '文件夹名称不能为空',
-									icon: 'none'
-								});
-							}
-							//模拟请求服务器，这里先增加到list数组中
-							this.list.push({
-								type: 'dir',
-								name: this.newdirname,
-								create_time: '2020-10-22 17:00',
-								checked: false
-							});
-							uni.showToast({
-								title: '新建文件夹成功',
-								icon: 'none'
-							});
-							close();
-						});
-						break;
-					default:
-						break;
+			],
+			addList: [
+				{
+					icon: 'icon-file-b-6',
+					color: 'text-success',
+					name: '上传图片'
+				},
+				{
+					icon: 'icon-file-b-9',
+					color: 'text-primary',
+					name: '上传视频'
+				},
+				{
+					icon: 'icon-file-b-8',
+					color: 'text-muted',
+					name: '上传文件'
+				},
+				{
+					icon: 'icon-file-b-2',
+					color: 'text-warning',
+					name: '新建文件夹'
 				}
+			]
+		};
+	},
+	onLoad() {},
+	methods: {
+		// 列表点击事件处理
+		doEvent(item) {
+			switch(item.type) {
+				case 'image': //预览图片
+				let images = this.list.filter(item => {
+					return item.type === 'image'
+				})
+				uni.previewImage({
+					current: item.data,
+					urls: images.map(item=>item.data)
+				})
+				   break;
+				default:
+				   break;
 			}
 		},
-		computed: {
-			//选中列表
-			checkList() {
-				return this.list.filter(item => item.checked);
-			},
-			//选中数量
-			checkCount() {
-				return this.checkList.length;
-			},
-			// 操作菜单
-			actions() {
-				if (this.checkCount > 1) {
-					return [{
-						icon: "icon-xiazai",
-						name: "下载"
-					}, {
-						icon: "icon-shanchu",
-						name: "删除"
-					}]
-				}
-				return [{
-					icon: "icon-xiazai",
-					name: "下载"
-				}, {
-					icon: "icon-fenxiang-1",
-					name: "分享"
-				}, {
-					icon: "icon-shanchu",
-					name: "删除"
-				}, {
-					icon: "icon-chongmingming",
-					name: "重命名"
-				}]
+		select(e) {
+			//接收到子组件传递过来的索引选中状态，将对应的list中的数据更新
+			this.list[e.index].checked = e.value;
+		},
+		//全选/取消全选
+		handleCheckAll(checked) {
+			this.list.forEach(item => {
+				item.checked = checked;
+			});
+		},
+		//处理底部操作条事件，这里仅对"删除"做了处理
+		handleBottomEvent(item) {
+			switch (item.name) {
+				case '删除':
+					this.$refs.dialog.open(close => {
+						//对list进行过滤，留下未被选中的
+						this.list = this.list.filter(item => !item.checked);
+						close();
+						uni.showToast({
+							title: '删除成功',
+							icon: 'none'
+						});
+						// // 在这儿可以写点击删除需要做的回调事件，这里先在控制台模拟，实际需要把checkList移除掉
+						// console.log('删除文件');
+						// console.log(this.checkList);
+					});
+					break;
+				case '重命名':
+					//重命名只能对单个文件进行，所以取this.checkList[0],也就是选中的唯一元素
+					this.renameValue = this.checkList[0].name;
+					this.$refs.rename.open(close => {
+						if (this.renameValue == '') {
+							return uni.showToast({
+								title: '文件名称不能为空',
+								icon: 'none'
+							});
+						}
+						//更新钙元素的name值，实时看到效果
+						this.checkList[0].name = this.renameValue;
+						close();
+					});
+					break;
+				default:
+					break;
+			}
+		},
+		//打开添加操作条
+		openAddDialog() {
+			this.$refs.add.open();
+		},
+		//处理添加操作条的各种事件
+		handleAddEvent(item) {
+			this.$refs.add.close();
+			switch (item.name) {
+				case '新建文件夹':
+					this.$refs.newdir.open(close => {
+						if (this.newdirname == '') {
+							return uni.showToast({
+								title: '文件夹名称不能为空',
+								icon: 'none'
+							});
+						}
+						//模拟请求服务器，这里先增加到list数组中
+						this.list.push({
+							type: 'dir',
+							name: this.newdirname,
+							create_time: '2020-10-22 17:00',
+							checked: false
+						});
+						uni.showToast({
+							title: '新建文件夹成功',
+							icon: 'none'
+						});
+						close();
+					});
+					break;
+				default:
+					break;
 			}
 		}
-	};
+	},
+	computed: {
+		//选中列表
+		checkList() {
+			return this.list.filter(item => item.checked);
+		},
+		//选中数量
+		checkCount() {
+			return this.checkList.length;
+		},
+		// 操作菜单
+		actions() {
+			if (this.checkCount > 1) {
+				return [
+					{
+						icon: 'icon-xiazai',
+						name: '下载'
+					},
+					{
+						icon: 'icon-shanchu',
+						name: '删除'
+					}
+				];
+			}
+			return [
+				{
+					icon: 'icon-xiazai',
+					name: '下载'
+				},
+				{
+					icon: 'icon-fenxiang-1',
+					name: '分享'
+				},
+				{
+					icon: 'icon-shanchu',
+					name: '删除'
+				},
+				{
+					icon: 'icon-chongmingming',
+					name: '重命名'
+				}
+			];
+		}
+	}
+};
 </script>
 
 <style></style>
