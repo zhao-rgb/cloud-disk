b57b3270478f9cc4057458aa81d53a2a6397ba9f
zhao-rgb <1019916061@qq.com>
2020-10-25 21:16:50 +0800

    个人中心和退出登录交互实现

--- store/index.js
+++ store/index.js
@@ -2,12 +2,29 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 
+import $H from '../common/request.js'
+
 export default new Vuex.Store({
 	state: {
 		user: null,
 		token: null
 	},
 	actions: {
+		logout({
+			state
+		}) {
+			$H.post('/logout', {}, {
+				token: true
+			})
+			state.user = null
+			state.token = null
+			uni.removeStorageSync('user')
+			uni.removeStorageSync('token')
+
+			uni.reLaunch({
+				url: '/pages/login/login'
+			})
+		},
 		login({
 			state
 		}, user) {
@@ -16,6 +33,21 @@ export default new Vuex.Store({
 
 			uni.setStorageSync('user', JSON.stringify(user))
 			uni.setStorageSync('token', user.token)
+		},
+		intUser({
+			state
+		}) {
+			let user = uni.getStorageSync('user')
+			if (user) {
+				state.user = JSON.parse(user)
+				state.token = state.user.token
+			}
+		},
+		updateSize({
+			state
+		}, e) {
+			state.user.total_size = e.total_size
+			state.user.user_size = e.used_size
 		}
 	}
 })
